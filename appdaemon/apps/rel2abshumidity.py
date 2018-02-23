228e397d75f138a27a35f3a43624440b6f919a70
balk77 <s.balkenende@gmail.com>
2018-02-23 17:24:22 +0100

    update 23 Feb 2018

---
+++ appdaemon/apps/rel2abshumidity.py
@@ -0,0 +1,17 @@
+import appdaemon.appapi as appapi
+import math
+
+class rel2abs(appapi.AppDaemon):
+    def initialize(self):
+        self.listen_state(self.inputhandler, self.args["relhumidity"])
+
+    def inputhandler(self, entity, attribute, old, new, kwargs):
+        temperature = float(self.get_state(self.args["temperature"]))
+        relhumidity = float(self.get_state(self.args["relhumidity"]))
+
+
+        abshumidity = round((6.112 * math.exp((17.67 * temperature)/(temperature+243.5)) * relhumidity * 2.1674)/(273.15+temperature),2);
+        # self.log("abshumidity")
+        # self.log(abshumidity)
+
+        status = self.set_state(self.args["abshumidity"], state=abshumidity)
