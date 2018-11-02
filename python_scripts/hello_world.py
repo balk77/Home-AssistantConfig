35417011ff7fd665ca70a375fdd8e121b8fd2c54
balk77 <s.balkenende@gmail.com>
2018-11-02 20:15:46 +0100

    deleted some files

--- python_scripts/hello_world.py
+++
@@ -1,6 +0,0 @@
-name = data.get('name', 'world')
-logger.info("Hello {}".format(name))
-hass.bus.fire("notify",{ "message" : "testtt" })
-#hass.bus.ServiceCall("notify","notify",{ "message" : "testtt" })
-#hass.bus.call_service("notify","notify",{ "message" : "testtt" })
-hass.bus.fire(name, { "wow": "from a Python script!" })
